0000620969dba9ee9de57ad474e869f1861e5082
Panji Gumelar <panjiggm@gmail.com>
2025-03-10 15:49:53 +0700

    add layout to landing

--- src/views/landing.tsx
+++ src/views/landing.tsx
@@ -4,6 +4,7 @@ import HeroFormSignUpForm from "@/components/hero";
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useCourses } from "@/hooks/use-courses";
+import SimpleLayout from "@/layout/SimpleLayout.tsx";
 import CourseCard from "@/sections/courses/CourseCard";
 import { useRouter } from "next/navigation";
 import React from "react";
@@ -24,35 +25,37 @@ const Landing = () => {
   const { data, isLoading, error } = useCourses(filters);
 
   return (
-    <div className="pb-10">
-      <HeroFormSignUpForm />
+    <SimpleLayout>
+      <div className="pb-10">
+        <HeroFormSignUpForm />
 
-      <div className="container max-w-7xl mx-auto p-8 md:p-0">
-        <div className="mb-4 flex justify-between items-center">
-          <h1 className="text-lg font-bold sm:text-xl md:text-2xl">
-            Course terbaru
-          </h1>
-          <Button
-            variant="link"
-            size={"sm"}
-            type="button"
-            className="underline text-blue-600 hover:text-blue-900"
-            onClick={() => router.push(`/courses`)}
-          >
-            Lihat semua Courses
-          </Button>
-        </div>
-        {isLoading ? (
-          <CoursesLoading />
-        ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {data?.courses.map((course) => (
-              <CourseCard key={course.id} course={course} />
-            ))}
+        <div className="container max-w-7xl mx-auto p-8 md:p-0">
+          <div className="mb-4 flex justify-between items-center">
+            <h1 className="text-lg font-bold sm:text-xl md:text-2xl">
+              Course terbaru
+            </h1>
+            <Button
+              variant="link"
+              size={"sm"}
+              type="button"
+              className="underline text-blue-600 hover:text-blue-900"
+              onClick={() => router.push(`/courses`)}
+            >
+              Lihat semua Courses
+            </Button>
           </div>
-        )}
+          {isLoading ? (
+            <CoursesLoading />
+          ) : (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+              {data?.courses.map((course) => (
+                <CourseCard key={course.id} course={course} />
+              ))}
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </SimpleLayout>
   );
 };
 
