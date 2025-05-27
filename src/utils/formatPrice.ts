ff6d2577ff3ccfc2e943d2cd0a2560a4afdc1214
panjiggm <panjiggm@gmail.com>
2025-05-27 16:30:20 +0700

    if price 0 text change to GRATIS

--- src/utils/formatPrice.ts
+++ src/utils/formatPrice.ts
@@ -1,4 +1,7 @@
 export const formatPrice = (price: number) => {
+  if (price === 0) {
+    return "GRATIS";
+  }
   return new Intl.NumberFormat("id-ID", {
     style: "currency",
     currency: "IDR",
