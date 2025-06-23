f9f335ac8bd7e7c551e15d7bfef51f64c4890020
panjiggm <panjiggm@gmail.com>
2025-06-23 11:30:54 +0700

    meta for dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,11 @@
 import Dashboard from "@/views/dashboard";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard Peserta | E-Learning",
+  description: "Your personal dashboard",
+  keywords: ["dashboard", "e-learning", "peserta"],
+};
 
 export default async function DashboardPage() {
   return <Dashboard />;
