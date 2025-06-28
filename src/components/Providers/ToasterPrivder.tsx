cfb89da2229ed3380db6b03514ece91df98542c0
panjiggm <panjiggm@gmail.com>
2025-06-28 10:30:47 +0700

    update toaster position

--- src/components/Providers/ToasterPrivder.tsx
+++ src/components/Providers/ToasterPrivder.tsx
@@ -1,16 +1,21 @@
-"use client"
+"use client";
 
-import { useTheme } from 'next-themes';
-import React from 'react'
+import { useTheme } from "next-themes";
+import React from "react";
 import { Toaster } from "sonner";
 
 const ToasterPrivder = () => {
-  const {theme} = useTheme();
-  const current_theme = theme == "dark" ? "dark" : "light"
+  const { theme } = useTheme();
+  const current_theme = theme == "dark" ? "dark" : "light";
 
   return (
-    <Toaster richColors={true} closeButton theme={current_theme} />
-  )
-}
+    <Toaster
+      position="top-right"
+      richColors={true}
+      closeButton
+      theme={current_theme}
+    />
+  );
+};
 
-export default ToasterPrivder
+export default ToasterPrivder;
