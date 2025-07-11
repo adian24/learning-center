0954fb92b20b15acf055bbcb3499ea3369a3303f
Panji Gumelar <panjiggm@gmail.com>
2025-07-11 17:18:12 +0700

    hapus skeleton

--- src/views/courses/index.tsx
+++ src/views/courses/index.tsx
@@ -3,34 +3,6 @@ import SimpleLayout from "@/layout/SimpleLayout.tsx";
 import CoursesClient from "@/sections/courses/CourseClient";
 import { Suspense } from "react";
 
-const CoursesLoading = () => (
-  <div className="space-y-4">
-    <div className="container max-w-max mx-auto py-8">
-      <div className="mb-8">
-        <Skeleton className="h-10 w-1/3 mb-2" />
-        <Skeleton className="h-5 w-2/3" />
-      </div>
-      <Skeleton className="h-12 w-full mb-6" />
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {[...Array(6)].map((_, index) => (
-          <div key={index} className="border rounded-lg overflow-hidden">
-            <Skeleton className="h-48 w-full" />
-            <div className="p-4 space-y-3">
-              <Skeleton className="h-6 w-3/4" />
-              <Skeleton className="h-4 w-full" />
-              <Skeleton className="h-4 w-full" />
-              <div className="flex justify-between pt-2">
-                <Skeleton className="h-4 w-1/4" />
-                <Skeleton className="h-4 w-1/4" />
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  </div>
-);
-
 const Courses = () => {
   return (
     <SimpleLayout>
