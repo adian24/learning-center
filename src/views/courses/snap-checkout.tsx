eea539f786b8dfafeb4a2c05cd94a4b2dbb467a3
Panji Gumelar <panjiggm@gmail.com>
2025-07-21 13:30:29 +0700

    snapcheckout layout fix

--- src/views/courses/snap-checkout.tsx
+++ src/views/courses/snap-checkout.tsx
@@ -26,7 +26,7 @@ export default function SnapCheckoutPage({
   user,
 }: SnapCheckoutPageProps) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-6xl mx-auto">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 max-w-4xl mx-auto p-4">
       <div>
         <SnapCheckout
           courseId={course.id}
